8a5ac742c83bd2074f501c4d30693603c48aeec0
Mostafa Essam <102423748+MrQuartz99@users.noreply.github.com>
2025-09-24 02:03:17 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,34 +4,13 @@ using namespace std;
 #include "lib/Image_Class.h"
 
 int main() {
-    string filename;
+   /* string filename;
     cin >> filename;
 
     Image image("images/" + filename);
+    image.saveImage("generated/" + filename);*/
 
-    for (int i = 0; i < image.width; ++i) {
-        for (int j = 0; j < image.height; ++j) {
-            unsigned int avg = 0;
-
-            for (int k = 0; k < 3; ++k) {
-                avg += image(i, j, k);
-            }
-            avg /= 3;
-
-            unsigned int bw = (avg >= 128) ? 255 : 0;
-
-            image(i, j, 0) = bw;
-            image(i, j, 1) = bw;
-            image(i, j, 2) = bw;
-        }
-    }
-
-
-    cout << "Pls enter image name to store new image\n";
-    cout << "and specify extension .jpg, .bmp, .png, .tga: ";
-
-    cin >> filename;
-    image.saveImage("generated/" + filename);
+    cout << "Heya!" << endl;
 
     return 0;
 }
