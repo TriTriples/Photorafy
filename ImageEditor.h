ec056e7ab7303c869828869a0beebf78343c1330
Mostafa Essam <102423748+MrQuartz99@users.noreply.github.com>
2025-10-01 01:30:13 +0300

    Update ImageEditor.h

--- ImageEditor.h
+++ ImageEditor.h
@@ -4,12 +4,14 @@
 #include <string>
 #include <stack>
 #include "lib/Image_Class.h"
+#include <vector>
 
 class ImageEditor {
 private:
     Image originalImage;       
     Image currentImage;
     std::stack<Image> history;
+    std::vector<int> supportedAngles = {0, 90, -90, 180, 270, -270};
 
 public:
     ImageEditor(const std::string& path);
@@ -22,12 +24,12 @@ public:
     void changeBrightness(double factor);
     void resizeImage(const Image& src, int newW, int newH);
     void mergeWithImage(const std::string& imagePath);
+    void rotate(int angleDegrees);
 
     void undo();
     void save(const std::string& path);
     void preview(const std::string& path);
-
-
+    const std::vector<int>& getSupportedAngles() const { return supportedAngles; }
     const Image& getImage() const { return currentImage; }
 };
 
