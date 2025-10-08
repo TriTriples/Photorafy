607c2c1f324decf493fc2ad0f83ab072722a8420
Mostafa Essam <102423748+MrQuartz99@users.noreply.github.com>
2025-10-09 02:07:27 +0300

    Update ImageEditor.h

--- ImageEditor.h
+++ ImageEditor.h
@@ -12,6 +12,7 @@ private:
     Image currentImage;
     std::stack<Image> history;
     std::vector<int> supportedAngles = {0, 90, -90, 180, 270, -270};
+    std::vector<std::string> supportedFormats = {"jpg", "jpeg", "png", "bmp", "tga"};
 
 public:
     ImageEditor(const std::string& path);
@@ -22,18 +23,27 @@ public:
     void grayscale();
     void invert();
     void changeBrightness(double factor);
-    void resizeImage(const Image& src, int newW, int newH);
+    Image resizeImage(const Image& src, int newW, int newH, bool forMerge = false);
     void mergeWithImage(const std::string& imagePath);
     void rotate(int angleDegrees);
-    void boxBlur(int radius);
+    int calculateSuitableThickness();
+    void addFrame(int thickness, unsigned char r = 0, unsigned char g = 0, unsigned char b = 0, bool decorated = false);
+    void edgeDetection();
+    void purpleFilter();
+    void sunlightFilter(double intensity = 0.02);
     void oldTVFilter(double noiseLevel = 0.15, double scanlineIntensity = 0.7, double distortionLevel = 20.0);
     void skewImage(double skewDegree);
+    void boxBlur(int radius = 5);
+    void crop(int startX, int startY, int width, int height);
+    void oilPainting(int radius = 3, int intensityLevels = 10);
 
     void undo();
     void save(const std::string& path);
     void preview(const std::string& path);
-    const std::vector<int>& getSupportedAngles() const { return supportedAngles; }
-    const Image& getImage() const { return currentImage; }
+    Image getCurrentImage() const;
+    bool isValidImageFormat(const std::string& filename);
+    std::string promptForValidFilename(const std::string& defaultName);
+    std::vector<int> getSupportedAngles() const;
 };
 
 #endif
